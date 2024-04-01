948c8a2b566075b67538620bb3ce4619ad2e4c9e
leonardoolia <leonardooliaa@gmail.com>
2024-04-01 17:33:59 +0200

    added pagination

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,38 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import AppAlert from './components/AppAlert.vue';
 import axios from 'axios';
-const endpoint = 'http://localhost:8000/api/projects/';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
 
   export default{
     name: 'MyPortfolio',
-    components:{AppHeader, ProjectsList},
-    data: () => ({projects: [], isLoading: false}),
+    components:{AppHeader, ProjectsList, AppAlert},
+    data: () => ({
+      projects: {
+        data: [],
+        links: []
+      }, 
+      isLoading: false, 
+      isAlertOpen: false}),
     methods:{      
-      fetchProjects(){
+      fetchProjects(endpoint){
         this.isLoading = true;
-        axios.get(endpoint)
+        axios.get(endpoint ?? defaultEndpoint)
         .then(res => {
-          this.projects = res.data
+          const {data, links} = res.data;
+          this.projects = {data, links};
+
+          this.isAlertOpen = false;
         })
         .catch(err =>{
-          console.error(err)
+          console.error(err);
+          this.isAlertOpen = true;
         })
         .then(() =>{
           this.isLoading = false;
         })
-      }
+      }      
     },
     created(){
       this.fetchProjects();
@@ -35,9 +46,17 @@ const endpoint = 'http://localhost:8000/api/projects/';
   <AppHeader />
   <main class="container mt-4">
     <h1 class="mb-4">MyPortfolio</h1>
-
+    <AppAlert :show="isAlertOpen" @close="isAlertOpen = false" @retry="fetchProjects"/>
     <AppLoader v-if="isLoading"/>
-    <ProjectsList v-else :projects="projects"/>
+    <ProjectsList v-else :projects="projects.data"/>
+
+    <nav>
+      <ul class="pagination">
+        <li v-for="link in projects.links" :key="link.label" class="page-item" :class="{'active': link.active, 'disabled': !link.url}">
+          <button class="page-link" v-html="link.label" :disabled="!link.url" @click="fetchProjects(link.url)"></button>
+        </li>
+      </ul>
+    </nav>
   </main>
 
 </template>
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,21 @@
+<!--? SCRIPT -->
+<script>
+  export default{
+    name: 'AppAlert',
+    props: {show: Boolean},
+    emits: ['close', 'retry']
+  };
+</script>
+
+<!--? TEMPLATE -->
+<template>
+    <div v-if="show" class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong></strong> Si è verificato un errore. <strong class="text-decoration-underline" role="button" @click="$emit('retry')">Riprova</strong>
+        <button type="button" class="btn-close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<!--? STYLE -->
+<style>
+
+</style>
