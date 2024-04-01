928380491dd5f3b44da8cb7416fff0bb0c5bce32
leonardoolia <leonardooliaa@gmail.com>
2024-04-01 13:58:20 +0200

    called projects from db through api

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
+<!--? SCRIPT -->
 <script>
-
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
 
   export default{
     name: 'MyPortfolio',
-    components:{AppHeader}
+    components:{AppHeader},
+    data: () => ({projects: []}),
+    methods:{
+      fetchProjects(){
+        axios.get(endpoint).then(res => {
+          this.projects = res.data
+        })
+      }
+    },
+    created(){
+      this.fetchProjects();
+    }
   };
-
 </script>
 
+<!--? TEMPLATE -->
 <template>
   <AppHeader />
   <main class="container mt-4">
     <h1>MyPortfolio</h1>
-  </main>
 
+    <section id="projects">
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <h4 v-text="project.title"></h4>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
+<!--? STYLE -->
 <style>
 
 </style>
