5f9f040ecbc70b9ce7c99265340182b348641a04
leonardoolia <leonardooliaa@gmail.com>
2024-04-02 16:06:37 +0200

    fixed project detail page

--- src/components/pages/ProjectDetailPage.vue
+++ src/components/pages/ProjectDetailPage.vue
@@ -3,19 +3,35 @@
 import axios from 'axios';
 import AppLoader from '../AppLoader.vue';
 import ProjectCard from '../projects/ProjectCard.vue';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
+
 export default{
   name: 'ProjectDetailPage',
   components: {ProjectCard, AppLoader},
   data: () => ({
+    project: null,
     isLoading: false,
-  })
-}
+  }),
+  methods:{
+    getProject(){
+        this.isLoading = true;
+        // axios.get(`${defaultEndpoint}/1`)
+        axios.get(defaultEndpoint + this.$route.params.id)
+        .then(res =>{this.project = res.data})
+        .catch(err =>{console.error(err.message)})
+        .then(() => {this.isLoading = false})
+    },
+  },
+  created(){
+        this.getProject();
+    }
+};
 </script>
 
 <!--? TEMPLATE -->
 <template>
-    <AppLoader v-if="isLoading"/>
-    <ProjectCard v-else/>
+    <AppLoader v-if="isLoading && !project"/>
+    <ProjectCard v-if="!isLoading && project" :project="project"/>
 </template>
 
 <!--? STYLE -->
