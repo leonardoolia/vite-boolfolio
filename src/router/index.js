7913c09c5c899232f3da25d25134fdacecaaec3f
leonardoolia <leonardooliaa@gmail.com>
2024-04-02 14:49:33 +0200

    added routes to homepage and contact us page

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from '../components/pages/HomePage.vue';
+import ContactUsPage from '../components/pages/ContactUsPage.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: HomePage, name: 'home'},
+        {path: '/contact-us', component: ContactUsPage, name: 'contact-us'},
+        {path: '/:pathMatch(.*)*', redirect:'/'},
+    ]
+});
+
+export {router}
