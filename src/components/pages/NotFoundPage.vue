97f1bcd017eafb74353516ea9b05aea46408d0c4
leonardoolia <leonardooliaa@gmail.com>
2024-04-02 15:01:35 +0200

    added not found page

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,18 @@
+<!--? SCRIPT -->
+<script>
+export default{    
+  name: 'NotFoundPage'  
+}
+</script>
+
+<!--? TEMPLATE -->
+<template>
+    <div class="vh-100 d-flex flex-column align-items-center justify-content-center">
+        <h3 class="text-white text-center">404 | Page not found</h3>
+    </div>
+</template>
+
+<!--? STYLE -->
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from '../components/pages/HomePage.vue';
 import ContactUsPage from '../components/pages/ContactUsPage.vue';
+import NotFoundPage from '../components/pages/NotFoundPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: HomePage, name: 'home'},
         {path: '/contact-us', component: ContactUsPage, name: 'contact-us'},
-        {path: '/:pathMatch(.*)*', redirect:'/'},
+        {path: '/:pathMatch(.*)*', component: NotFoundPage},
     ]
 });
 
